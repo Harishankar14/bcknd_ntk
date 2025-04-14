87ba69c42804c7e697ebd01aa46fc0adeb44b87b
Harishankar.1 <88716735+Harishankar14@users.noreply.github.com>
2025-04-14 11:40:45 +0530

    Update OCR.py

--- OCR.py
+++ OCR.py
@@ -1,24 +1,23 @@
-try:  
-    from PIL import Image
-    from googletrans import Translator
-except ImportError:  
-    import Image
-import pytesseract
+from ocr_tamil.ocr import OCR
+import langid
+from googletrans import Translator
 
+# Initialize OCR and translator once
+ocr = OCR(detect=True)
+translator = Translator()
 
-def ocr_core(filename):  
-    """
-    This function will handle the core OCR processing of images.
-    """
-    
-    text= pytesseract.image_to_string(Image.open(filename), lang='eng')  # We'll use Pillow's Image class to open the image and pytesseract to detect the string in the image
-    return text
+def ocr_core(image_path):
+    texts = ocr.predict(image_path)
+    extracted_text = " ".join(texts[0])
 
-def translatee(filename):
-    translator = Translator()
+    tamil_text = []
+    for word in extracted_text.split():
+        lang, _ = langid.classify(word)
+        if lang == 'ta':
+            tamil_text.append(word)
 
-   
-    text1= pytesseract.image_to_string(Image.open(filename), lang='eng')
-    translation_text=translator.translate( text1 , dest='tamil')
-    return translation_text
+    return " ".join(tamil_text)
 
+def translatee(tamil_text):
+    translated = translator.translate(tamil_text, src='ta', dest='hi')
+    return translated.text

ab02eeb07d0aa2400d421b091d958b24affa0bbf
Harishankar.1 <88716735+Harishankar14@users.noreply.github.com>
2025-04-14 11:49:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,30 +2,24 @@ import os
 from flask import Flask, render_template, request
 
 # Import OCR + translate functions from ocr.py
-from ocr_core import ocr_core, translatee
+from ocr_core import ocr_core, translate
 
-
-# Define a folder to store and later serve the images
 UPLOAD_FOLDER = 'static/uploads/'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-# Create the upload folder if it doesn't exist
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 
-# Function to check the file extension
 def allowed_file(filename):  
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-# Route for home page
 @app.route('/')
 def home_page():  
     return render_template('index.html')
 
-# Route for upload page
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_page():  
     if request.method == 'POST':
